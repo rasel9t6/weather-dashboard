f36f6b9632f5827dbb18c881b1e2d1a1aa0b387e
Rasel Hossen <raselhossen052@gmail.com>
2024-02-09 23:50:39 +0600

    setup/weather-dashboard

---
+++ src/Page.jsx
@@ -0,0 +1,11 @@
+import Header from './components/header/Header';
+import Weather from './components/weather/Weather';
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <Weather />
+    </>
+  );
+}
